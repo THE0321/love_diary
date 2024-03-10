6cca53b2601ab38d4fed7c1c276ecb3d563b3a69
THE0321 <89115975+THE0321@users.noreply.github.com>
2024-03-10 15:19:04 +0900

    init

---
+++ src/main/java/com/love/love_diary/LoveDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.love.love_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/love/love_diary/LoveDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love.love_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
